a3f9701bc97f2d47110d9944aaca32c41a696abb
nguyenngocphuongnb <nguyenngocphuongnb@264ee46a-0a5c-6511-0cbf-92145b4d8859>
2013-08-17 07:11:56 +0000

    Admin: Move student table function

--- trunk/modules/estudent/admin/create_student_table.php
+++ trunk/modules/estudent/admin/create_student_table.php
@@ -3,57 +3,6 @@
 
 if( !defined('STUDENT_FUNCTION') ) die('Stop!');
 
-$table_log = file( NV_ROOTDIR . '/modules/' . $module_file . '/data/student_table_data.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES );
 
-$table = $add_config['faculty_id'] . '_' . $add_config['year'];
-
-if( !in_array( $table , $table_log ) )
-{
-	$_sql_string = "CREATE TABLE `" . NV_PREFIXLANG . "_" . $module_data . "_student_" . $table . "` (
-	  `student_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  
-	  `userid` mediumint(8) NOT NULL DEFAULT '0',
-	  `faculty_id` mediumint(8) NOT NULL DEFAULT '0',
-	  `level_id` mediumint(8) NOT NULL DEFAULT '0',
-	  `base_class_id` varchar(255) NOT NULL,
-	  `student_name` varchar(255) NOT NULL,
-	  `student_code` varchar(255) NOT NULL,
-	  `course_id` varchar(255) NOT NULL,
-  	  `year` int(11) NOT NULL DEFAULT '0',
-	  
-	  `family_name` varchar(255),
-	  `last_name` varchar(255),
-	  `birthday` int(11) NOT NULL DEFAULT '0',
-	  `hometown` varchar(255) NOT NULL,
-	  `address` varchar(255) NOT NULL,
-	  `email` varchar(255) NOT NULL,
-	  `phone` varchar(255) NOT NULL,
-	  
-	  `study_result` longtext NOT NULL,
-	  `subject_registered` longtext NOT NULL,
-	  `class_registered` longtext NOT NULL,
-	  `subject_log` longtext NOT NULL,
-	  `off_class_count` longtext NOT NULL,
-	  
-	  `study_status` int(11) NOT NULL DEFAULT '0',
-	  
-	  `student_desc` mediumtext NOT NULL,
-	  `weight` smallint(4) NOT NULL DEFAULT '0',
-	  `admin_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-	  `add_time` int(11) NOT NULL DEFAULT '0',
-	  `edit_time` int(11) NOT NULL DEFAULT '0',
-	  `status` tinyint(1) unsigned NOT NULL DEFAULT '0',
-	  PRIMARY KEY (`student_id`),
-	  UNIQUE KEY `userid` (`userid`),
-	  UNIQUE KEY `student_code` (`student_code`)
-	) ENGINE=MyISAM";
-	
-	if( !$db->sql_query($_sql_string) ) die($_sql_string);
-	else
-	{
-		file_put_contents(NV_ROOTDIR . '/modules/' . $module_file . '/data/student_table_data.txt', $table . PHP_EOL, FILE_APPEND | LOCK_EX);
-		nv_del_moduleCache( $module_name );
-	}
-}
 
 ?>
--- trunk/modules/estudent/admin/student_functions.php
+++ trunk/modules/estudent/admin/student_functions.php
@@ -20,26 +20,50 @@ if( $studentid )
 {
 	$student['student_id'] = $studentid;
 	$_backUpStudent = VnpGetStudent($studentid);
-	if( VnpUpdateStudent( $student ) )
+	$new_table = '_' . $student['faculty_id'] . '_' . $student['year'];
+	$old_table = '_' . $_backUpStudent['faculty_id'] . '_' . $_backUpStudent['year'];
+	$_error = false;
+	if( $new_table != $old_table )
 	{
-		$table = '_' . $student['faculty_id'] . '_' . $student['year'];
-		if( VnpUpdateStudent( $student, $table ) )
+		$tableData = array( 'faculty_id' => $student['faculty_id'], 'year' => $student['year'] );
+		if( createStudentTable($tableData) )
 		{
-			$msg = array( 'content' => $lang_module['success_update_student'] . ' - ' . $student['student_id'] . ' - ' . $student['student_name'], 'type' => 'success' );
-			$logMsg = array( 'Success update student', $lang_module['success_update_student'] . ' - '  . $student['student_id'] . ' - ' . $student['student_name'] );
+			if( !VnpAddStudent( $_backUpStudent, $new_table ) )
+			{
+				$_error = true;
+				$msg = array( 'content' => $lang_module['error_student_table_move_student'] . ' - Student ID: ' . $student['student_id'] . ' - Table: ' . $old_table . ' ---> ' . $new_table, 'type' => 'error' );
+				$logMsg = array( 'Error move student ', $lang_module['error_student_table_move_student'] . ' - Student ID: ' . $student['student_id'] . ' - Table: ' . $old_table . ' ---> ' . $new_table );
+			}
 		}
 		else
 		{
-			VnpUpdateStudent( $_backUpStudent );
-			
-			$msg = array( 'content' => $lang_module['error_update_student'] . ' - '  . $student['student_id'] . ' - ' . $student['student_name'] . ' - ' . $_backUpStudent['faculty_id'] . '_' . $_backUpStudent['year'], 'type' => 'error' );
-			$logMsg = array( 'Error update student', $lang_module['error_update_student'] . ' - '  . $student['student_id'] . ' - ' . $student['student_name'] . ' - ' . $_backUpStudent['faculty_id'] . '_' . $_backUpStudent['year'] );
+			$_error = true;
+			$msg = array( 'content' => $lang_module['error_create_student_table'] . ' - ' . $new_table, 'type' => 'error' );
+			$logMsg = array( 'Error create student table', $lang_module['error_create_student_table'] . ' - '  . $new_table );
 		}
 	}
-	else
+	if( !$_error )
 	{
-		$msg = array( 'content' => $lang_module['error_update_student'] . ' - '  . $student['student_id'] . ' - ' . $student['student_name'] . ' - student', 'type' => 'error' );
-			$logMsg = array( 'Error update student', $lang_module['error_update_student'] . ' - '  . $student['student_id'] . ' - ' . $student['student_name'] . ' - student' );
+		if( VnpUpdateStudent( $student ) )
+		{
+			if( VnpUpdateStudent( $student, $new_table ) )
+			{
+				$msg = array( 'content' => $lang_module['success_update_student'] . ' - ' . $student['student_id'] . ' - ' . $student['student_name'], 'type' => 'success' );
+				$logMsg = array( 'Success update student', $lang_module['success_update_student'] . ' - '  . $student['student_id'] . ' - ' . $student['student_name'] );
+			}
+			else
+			{
+				VnpUpdateStudent( $_backUpStudent );
+				
+				$msg = array( 'content' => $lang_module['error_update_student'] . ' - '  . $student['student_id'] . ' - ' . $student['student_name'] . ' - ' . $_backUpStudent['faculty_id'] . '_' . $_backUpStudent['year'], 'type' => 'error' );
+				$logMsg = array( 'Error update student', $lang_module['error_update_student'] . ' - '  . $student['student_id'] . ' - ' . $student['student_name'] . ' - ' . $_backUpStudent['faculty_id'] . '_' . $_backUpStudent['year'] );
+			}
+		}
+		else
+		{
+			$msg = array( 'content' => $lang_module['error_update_student'] . ' - '  . $student['student_id'] . ' - ' . $student['student_name'] . ' - student', 'type' => 'error' );
+				$logMsg = array( 'Error update student', $lang_module['error_update_student'] . ' - '  . $student['student_id'] . ' - ' . $student['student_name'] . ' - student' );
+		}
 	}
 }
 else
@@ -72,105 +96,5 @@ else
 }
 
 nv_insert_logs( NV_LANG_DATA, $module_name, $logMsg[0], $logMsg[1], $admin_info['userid'] );
-
-function VnpGetStudent($studentid)
-{
-	global $db,$module_data, $admin_info;
-	
-	if( intval($studentid) > 0 )
-	{
-		$sql = "SELECT * FROM `" . NV_PREFIXLANG . "_" . $module_data . "_student` WHERE `student_id`=" . intval( $studentid );
-		$result = $db->sql_query( $sql );
-	
-		if( $db->sql_numrows( $result ) == 1 )
-		{
-			return $db->sql_fetchrow( $result );
-		}
-		else return false;
-	}
-	else return false;
-}
-
-function VnpUpdateStudent( $student, $table = '' )
-{
-	global $db,$module_data, $admin_info;
-	
-	$sql = "UPDATE`" . NV_PREFIXLANG . "_" . $module_data . "_student" . $table . "` SET
-			`userid` = " . intval( $student['userid'] ) . ",
-			`faculty_id` = " . intval( $student['faculty_id'] ) . ",
-			`level_id` = " . intval( $student['level_id'] ) . ",
-			`base_class_id` =" . $db->dbescape( $student['base_class_id'] ) . ",
-            `student_name` =" . $db->dbescape( $student['student_name'] ) . ",
-			`student_code` =" . $db->dbescape( $student['student_code'] ) . ",
-			`course_id` =" . $db->dbescape( $student['course_id'] ) . ",
-			`year` = " . intval( $student['year'] ) . ",
-			`birthday` = " . intval( $student['birthday'] ) . ",
-			`hometown` =" . $db->dbescape( $student['hometown'] ) . ",
-			`address` =" . $db->dbescape( $student['address'] ) . ",
-			`email` =" . $db->dbescape( $student['email'] ) . ",
-			`phone` =" . $db->dbescape( $student['phone'] ) . ",
-            `student_desc`= " .  $db->dbescape( $student['student_desc'] ) . ",
-			`edit_time`=" . NV_CURRENTTIME . " 
-	WHERE `student_id` =" . $student['student_id'];
-	$db->sql_query( $sql );
-	
-	if( $db->sql_affectedrows() > 0 )
-	{
-		return true;
-	}
-	else return false;
-}
-
-function VnpAddStudent( $student, $table = '' )
-{
-	global $db,$module_data, $admin_info;
-	
-	if( ! empty( $student['birthday'] ) and preg_match( "/^([0-9]{1,2})\/([0-9]{1,2})\/([0-9]{4})$/", $student['birthday'], $m ) )
-	{
-		$student['birthday'] = mktime( 0, 0, 0, $m[2], $m[1], $m[3] );
-	}
-	
-	( isset($student['student_id']) && $student['student_id'] > 0 ) ? $_stdID = $student['student_id'] : $_stdID = 'NULL';
-	
-	$sql = "INSERT INTO `" . NV_PREFIXLANG . "_" . $module_data . "_student" . $table . "` VALUES (
-            " . $_stdID . ",
-			" . intval( $student['userid'] ) . ",
-			" . intval( $student['faculty_id'] ) . ",
-			" . intval( $student['level_id'] ) . ",
-			" . $db->dbescape( $student['base_class_id'] ) . ",
-			" . $db->dbescape( $student['student_name'] ) . ",
-			" . $db->dbescape( $student['student_code'] ) . ",
-			" . $db->dbescape( $student['course_id'] ) . ",
-			" . intval( $student['year'] ) . ",
-			'',
-			'',
-			" . intval( $student['birthday'] ) . ",
-			" . $db->dbescape( $student['hometown'] ) . ",
-			" . $db->dbescape( $student['address'] ) . ",
-			" . $db->dbescape( $student['email'] ) . ",
-			" . $db->dbescape( $student['phone'] ) . ",
-			'',
-			'',
-			'',
-			'',
-			'',
-			1,
-			" . $db->dbescape( $student['student_desc'] ) . ",
-			0,
-			" . $admin_info['admin_id'] . ",
-			" . NV_CURRENTTIME . ",
-			" . NV_CURRENTTIME . ", 1);";
-	if( $_id = $db->sql_query_insert_id( $sql ) ) return $_id;
-	else return 0;
-}
-
-function VnpDeleteStudent( $stdID, $table = '' )
-{
-	global $db, $module_data;
-	
-	$sql = "DELETE FROM `" . NV_PREFIXLANG . "_" . $module_data . "_student" . $table . "` WHERE `student_id`=" . intval($stdID);
-	if( $db->sql_query($sql) ) return true;
-	else return false;
-}
 
 ?>
