bcd038ce0c810e1c8681ed0b448ab91b4eb8c293
nguyenngocphuongnb <nguyenngocphuongnb@264ee46a-0a5c-6511-0cbf-92145b4d8859>
2013-09-23 02:43:04 +0000

    Add user notification

--- trunk/modules/estudent/functions.php
+++ trunk/modules/estudent/functions.php
@@ -13,9 +13,44 @@ define( 'NV_IS_MOD_ESTUDENT', true );
 include NV_ROOTDIR . '/modules/' . $module_file . '/global.functions.php';
 
 $userData = checkUserPosition($user_info['userid']);
+$userInfo = array();
 if( $op == 'manage' )
 {
 	//p(checkUserPosition(3));
+}
+
+$userInfo['type'] = $userData['type'];
+if( $userData['type'] == 'teacher' )
+{
+	$sql = "SELECT * FROM `" . NV_PREFIXLANG . "_" . $module_data . "_class` WHERE ( `teacher_id`='" . $userData['teacher_id'] . "' OR `teacher_id` REGEXP '^" . $userData['teacher_id'] . "\\\,' OR `teacher_id` REGEXP '\\\," . $userData['teacher_id'] . "\\\,' OR `teacher_id` REGEXP '\\\," . $userData['teacher_id'] . "\$') AND `faculty_id`=" . $userData['faculty_id'] . "  AND `enter_mark`=1";
+	$result = $db->sql_query( $sql );
+	$required_mark = $db->sql_numrows( $result );
+	
+	
+	$userInfo['info'] = 'Khoa: ' . $globalTax['faculty'][$userData['faculty_id']]['faculty_name'];
+
+	$userInfo['type'] = $lang_module['teacher'];
+	$userInfo['name'] = $userData['teacher_name'];
+	$userInfo['mark']['required_mark'] = 0;
+	if( $required_mark > 0 )
+	{
+		$userInfo['mark']['required_mark'] = $required_mark;
+		$userInfo['mark']['required_mark_link'] = NV_BASE_SITEURL . 'index.php?' . NV_NAME_VARIABLE . "=" . $module_name . '&amp;' . NV_OP_VARIABLE . '=manage/mark/';
+	}
+}
+elseif( $userData['type'] == 'student' )
+{
+	$base_class = getBaseClass($userData['base_class_id']);
+	$base_class = $base_class[$userData['base_class_id']];
+	$userInfo['info'] = 'Lớp: ' . $base_class['base_class_name'] . '<br /> Khoa: ' . $globalTax['faculty'][$userData['faculty_id']]['faculty_name'];
+	$userInfo['type'] = $lang_module['student'];
+	$userInfo['name'] = $userData['student_name'];
+}
+else
+{
+	$userInfo['info'] = 'Vui lòng đăng nhập để sử dụng chức năng này!';
+	$userInfo['type'] = 'Khách';
+	$userInfo['name'] = 'Khách';
 }
 
 
--- trunk/modules/estudent/theme.php
+++ trunk/modules/estudent/theme.php
@@ -11,10 +11,19 @@ if( ! defined( 'NV_IS_MOD_ESTUDENT' ) ) die( 'Stop!!!' );
 
 function vnp_page_content($menu, $vnp_content)
 {
-	global $module_file, $lang_module, $module_info;
+	global $module_file, $lang_module, $module_data, $module_info, $userInfo, $db, $page_title, $module_name;
 
 	$xtpl = new XTemplate( "manage.tpl", NV_ROOTDIR . "/themes/" . $module_info['template'] . "/modules/" . $module_file );
 	$xtpl->assign( 'LANG', $lang_module );
+	
+	$info = array();
+	
+	$xtpl->assign( 'INFO', $userInfo );
+	if( isset($userInfo['mark']) && $userInfo['mark']['required_mark'] > 0 )
+	{
+		$page_title = '(' . $userInfo['mark']['required_mark'] . ') ' . $page_title;
+		$xtpl->parse( 'main.required_mark' );
+	}
 	
 	if( !empty( $menu ) )
 	{
